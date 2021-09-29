77022eca2f9df10c88ed3c758a7041ffd350d110
ElChad17 <91637933+ElChad17@users.noreply.github.com>
2021-09-29 23:04:09 +0100

    Add files via upload

---
+++ wipe.py
@@ -0,0 +1,31 @@
+import os
+
+def getFiles():
+    files= os.listdir()
+    for i in files:
+        if '.osu' in i:
+            textFileName = i.replace('.osu','.txt')
+            os.rename(i,textFileName)
+            try:
+                text = open(textFileName,'r',errors="ignore").read().splitlines()
+                if text[9] != 'Mode: 0':
+                    os.remove(textFileName)
+                    print('Removed',i)
+                else:
+                    os.rename(textFileName,i)
+                
+            except Exception as e:
+                print(e)
+                os.rename(textFileName,i)
+                print(i)
+                print()
+
+
+folders = os.listdir()
+for i in folders:
+    if '.zip' not in i:
+        os.chdir(i)
+        getFiles()
+        os.chdir('..')
+
+
